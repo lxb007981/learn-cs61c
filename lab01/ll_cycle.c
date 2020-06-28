b35a1bbb61473fa7bd2bb731f88178fe898347f1
lxb007981 <lxb007981@hotmail.com>
2020-06-28 15:51:08 +0800

    "first commit"

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -1,7 +1,22 @@
 #include <stddef.h>
 #include "ll_cycle.h"
 
-int ll_has_cycle(node *head) {
+int ll_has_cycle(node *head)
+{
     /* your code here */
+    if (!head || !head->next) //head is null or head has no next
+    {
+        return 0;
+    }
+    node *tortoise, hare = head;
+    while (hare->next && hare->next->next)
+    {
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+        if (tortoise = hare)
+        {
+            return 1;
+        }
+    }
     return 0;
 }
