7d632a3f0508c5aaaf94ae8aa821a18e55df738c
lxb <lxb007981@hotmail.com>
2020-08-23 21:01:21 +0800

    lab07

--- lab07/transpose.c
+++ lab07/transpose.c
@@ -4,9 +4,12 @@
 #include <time.h>
 
 /* The naive transpose function as a reference. */
-void transpose_naive(int n, int blocksize, int *dst, int *src) {
-    for (int x = 0; x < n; x++) {
-        for (int y = 0; y < n; y++) {
+void transpose_naive(int n, int blocksize, int *dst, int *src)
+{
+    for (int x = 0; x < n; x++)
+    {
+        for (int y = 0; y < n; y++)
+        {
             dst[y + x * n] = src[x + y * n];
         }
     }
@@ -14,46 +17,70 @@ void transpose_naive(int n, int blocksize, int *dst, int *src) {
 
 /* Implement cache blocking below. You should NOT assume that n is a
  * multiple of the block size. */
-void transpose_blocking(int n, int blocksize, int *dst, int *src) {
+void transpose_blocking(int n, int blocksize, int *dst, int *src)
+{
     // YOUR CODE HERE
-}
-
-void benchmark(int *A, int *B, int n, int blocksize,
-    void (*transpose)(int, int, int*, int*), char *description) {
-
-    int i, j;
-    printf("Testing %s: ", description);
-
-    /* initialize A,B to random integers */
-    srand48( time( NULL ) );
-    for( i = 0; i < n*n; i++ ) A[i] = lrand48( );
-    for( i = 0; i < n*n; i++ ) B[i] = lrand48( );
-
-    /* measure performance */
-    struct timeval start, end;
-
-    gettimeofday( &start, NULL );
-    transpose( n, blocksize, B, A );
-    gettimeofday( &end, NULL );
-
-    double seconds = (end.tv_sec - start.tv_sec) +
-        1.0e-6 * (end.tv_usec - start.tv_usec);
-    printf( "%g milliseconds\n", seconds*1e3 );
-
-
-    /* check correctness */
-    for( i = 0; i < n; i++ ) {
-        for( j = 0; j < n; j++ ) {
-            if( B[j+i*n] != A[i+j*n] ) {
-                printf("Error!!!! Transpose does not result in correct answer!!\n");
-                exit( -1 );
+    for (int x = 0; x < n; x += blocksize)
+    {
+        for (int y = 0; y < n; y += blocksize)
+        {
+            for (int z = 0; z < blocksize; z++)
+            {
+                for (int t = 0; t < blocksize; t++)
+                {
+                    if ((x + z) < n && (y + t) < n && (y + t) < n && (x + z) < n)
+                    {
+                        dst[x + z + n * (y + t)] = src[y + t + n * (x + z)];
+                    }
+                }
             }
         }
     }
 }
 
-int main( int argc, char **argv ) {
-    if (argc != 3) {
+void benchmark(int *A, int *B, int n, int blocksize,
+               void (*transpose)(int, int, int *, int *), char *description)
+{
+
+    int i, j;
+    printf("Testing %s: ", description);
+
+    /* initialize A,B to random integers */
+    srand48(time(NULL));
+    for (i = 0; i < n * n; i++)
+        A[i] = lrand48();
+    for (i = 0; i < n * n; i++)
+        B[i] = lrand48();
+
+    /* measure performance */
+    struct timeval start, end;
+
+    gettimeofday(&start, NULL);
+    transpose(n, blocksize, B, A);
+    gettimeofday(&end, NULL);
+
+    double seconds = (end.tv_sec - start.tv_sec) +
+                     1.0e-6 * (end.tv_usec - start.tv_usec);
+    printf("%g milliseconds\n", seconds * 1e3);
+
+    /* check correctness */
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (B[j + i * n] != A[i + j * n])
+            {
+                printf("Error!!!! Transpose does not result in correct answer!!\n");
+                exit(-1);
+            }
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
         printf("Usage: transpose <n> <blocksize>\nExiting.\n");
         exit(1);
     }
@@ -62,15 +89,15 @@ int main( int argc, char **argv ) {
     int blocksize = atoi(argv[2]);
 
     /* allocate an n*n block of integers for the matrices */
-    int *A = (int*)malloc( n*n*sizeof(int) );
-    int *B = (int*)malloc( n*n*sizeof(int) );
+    int *A = (int *)malloc(n * n * sizeof(int));
+    int *B = (int *)malloc(n * n * sizeof(int));
 
     /* run tests */
     benchmark(A, B, n, blocksize, transpose_naive, "naive transpose");
     benchmark(A, B, n, blocksize, transpose_blocking, "transpose with blocking");
 
     /* release resources */
-    free( A );
-    free( B );
+    free(A);
+    free(B);
     return 0;
 }
