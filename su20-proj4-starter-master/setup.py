005843bb3feeb18623f7cbafdb7844343a5d6d3c
lxb007981 <lxb007981@hotmail.com>
2020-08-27 21:26:14 +0800

    finish task2

--- su20-proj4-starter-master/setup.py
+++ su20-proj4-starter-master/setup.py
@@ -7,6 +7,15 @@ def main():
     # Use the setup function we imported and set up the modules.
     # You may find this reference helpful: https://docs.python.org/3.6/extending/building.html
     # TODO: YOUR CODE HERE
-
+    module = Extension("numc",
+                       sources=['numc.c', 'matrix.c'],
+                       include_dirs=['/usr/include/python3.6m'],
+                       extra_compile_args=CFLAGS,
+                       extra_link_args=LDFLAGS)
+    setup(name='numc',
+          version='0.1',
+          description='Custom matrix library for CS61C',
+          author='lxb007981',
+          ext_modules=[module])
 if __name__ == "__main__":
     main()
